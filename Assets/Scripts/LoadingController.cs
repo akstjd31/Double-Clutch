1921c51bdad0f7fac54141b821915c7d3777bed0
Manseong Han <akstjd31@naver.com>
2026-02-12 14:48:49 +0900

    chore : 주석 추가

--- Assets/Scripts/LoadingController.cs
+++ Assets/Scripts/LoadingController.cs
@@ -6,6 +6,7 @@ public class LoadingController : Singleton<LoadingController>
 {
     private IEnumerator Start()
     {
+        // 임시 대기 시간 적용
         yield return new WaitForSeconds(2f);
 
         var target = GameManager.Instance.NextSceneName;
@@ -14,11 +15,9 @@ public class LoadingController : Singleton<LoadingController>
         op.allowSceneActivation = true;
 
         yield return op;      // 로드 + 활성화 완료까지 대기
-        yield return null;    // 다음 씬 첫 프레임 보장(선택이지만 추천)
 
         GameManager.Instance.NotifyLoadingDone();
 
-        // 로딩 컨트롤러는 한 번 쓰고 제거해도 됨
         Destroy(this.gameObject);
     }
 }
