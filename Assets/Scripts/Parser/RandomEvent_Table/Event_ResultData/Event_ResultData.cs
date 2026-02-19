7ea2b6191bb4bf9bf38ba2aaeaaa0c47ca00b2ba
gpwn145 <gpwn145@gmail.com>
2026-02-19 19:03:41 +0900

    feat : 랜덤이벤트 파싱
    RandomEvent_Table
    - Event_ChoiceData
    - Event_EventDataModel
    - Event_Localization
    - Event_ResultData
    - Event_ScriptSelector
    - Event_SoundData

---
+++ Assets/Scripts/Parser/RandomEvent_Table/Event_ResultData/Event_ResultData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public struct Event_ResultData
+{
+    public string resultId;
+    public string scriptId;
+    public string matchPersonalityId;
+    public int outcomeGrade;
+    public potentialType potentialChangeType;
+
+    public int potentialChangeValue;
+    public int conditionChange;
+    public string statusChange;
+    public string reactionPortraitId;
+    public string resultscriptKey;
+
+    public string seId;
+    public string floatingText;
+
+    public Event_ResultData(
+        string resultId, string scriptId, string matchPersonalityId, int outcomeGrade, potentialType potentialChangeType,
+        int potentialChangeValue, int conditionChange, string statusChange, string reactionPortraitId, string resultscriptKey,
+        string seId, string floatingText
+        )
+    {
+        this.resultId = resultId;
+        this.scriptId = scriptId;
+        this.matchPersonalityId = matchPersonalityId;
+        this.outcomeGrade = outcomeGrade;
+        this.potentialChangeType = potentialChangeType;
+
+        this.potentialChangeValue = potentialChangeValue;
+        this.conditionChange = conditionChange;
+        this.statusChange = statusChange;
+        this.reactionPortraitId = reactionPortraitId;
+        this.resultscriptKey = resultscriptKey;
+
+        this.seId = seId;
+        this.floatingText = floatingText;
+    }
+}
---
+++ Assets/Scripts/Parser/RandomEvent_Table/Event_ScriptSelector/Event_ScriptSelectorReader.cs
@@ -0,0 +1,67 @@
+﻿using GoogleSheetsToUnity;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Event_ScriptSelectorReader", menuName = "Scriptable Object/Event_ScriptSelectorReader", order = int.MaxValue)]
+public class Event_ScriptSelectorReader : DataReaderBase
+{
+    [SerializeField] public List<Event_ScriptSelectorData> DataList = new List<Event_ScriptSelectorData>();
+
+    internal void UpdateStats(List<GSTU_Cell> list, int rowIndex)
+    {
+        int selectorId = 0;
+        string eventId = null;
+        coreType selectCoreType = default;
+        string scriptId = null;
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            string col = list[i].columnId;
+            string val = list[i].value;
+
+            if (string.IsNullOrWhiteSpace(val) || val == "-")
+                val = "";
+
+            switch (col)
+            {
+                case "selectorId":
+                    int.TryParse(val, out selectorId);
+                    break;
+
+                case "coreType":
+                    if (!string.IsNullOrEmpty(val))
+                    {
+                        if (int.TryParse(val, out var eInt)) selectCoreType = (coreType)eInt;
+                        else if (Enum.TryParse(val, true, out coreType e)) selectCoreType = e;
+                    }
+                    break;
+
+                case "eventId":
+                    eventId = val;
+                    break;
+
+                case "scriptId":
+                    scriptId = val;
+                    break;
+            }
+        }
+
+
+        if (selectorId <= 0) return;
+
+        var synergyData = new Event_ScriptSelectorData
+        (
+            selectorId, eventId, selectCoreType, scriptId
+        );
+
+        DataList.Add(synergyData);
+    }
+
+    private static bool ParseBool(string val)
+    {
+        if (string.IsNullOrEmpty(val)) return false;
+        val = val.Trim().ToLowerInvariant();
+        return val == "1" || val == "true" || val == "y" || val == "yes";
+    }
+}
