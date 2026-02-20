72ec8c6358d8829573b939d8952d051e2636dde8
Manseong Han <akstjd31@naver.com>
2026-02-20 11:33:30 +0900

    chore : 메인 -> 로비 이동 방식 변경
    - 기존: Update에서 Input으로 처리
    - 변경됨: StartButton 리스너 추가

--- Assets/Scripts/UI/MainUI.cs
+++ Assets/Scripts/UI/MainUI.cs
@@ -3,31 +3,36 @@ using UnityEngine.UI;
 
 public class MainUI : MonoBehaviour
 {
+    [SerializeField] private Button _startButton;
 
     private void Awake()
     {
+        if (_startButton != null)
+        {
+            _startButton.onClick.AddListener(OnClickGameStart);
+        }
     }
 
     private void Update()
     {
         // 모바일 터치
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
+        // if (Input.touchCount > 0)
+        // {
+        //     Touch touch = Input.GetTouch(0);
 
-            if (touch.phase == TouchPhase.Began)
-            {
-                OnClickGameStart();
-            }
-        }
+        //     if (touch.phase == TouchPhase.Began)
+        //     {
+        //         OnClickGameStart();
+        //     }
+        // }
 
-#if UNITY_EDITOR
-        // 테스트용 (PC)
-        if (Input.GetMouseButtonDown(0))
-        {
-            OnClickGameStart();
-        }
-#endif
+// #if UNITY_EDITOR
+//         // 테스트용 (PC)
+//         if (Input.GetMouseButtonDown(0))
+//         {
+                // OnClickGameStart();
+//         }
+// #endif
 
     }
 
