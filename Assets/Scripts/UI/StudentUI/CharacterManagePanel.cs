9bfff21933a3a7548c2cda0822669767da06eef3
Manseong Han <akstjd31@naver.com>
2026-02-20 11:05:17 +0900

    fixed : 뒤로가기 버튼 수정

--- Assets/Scripts/UI/StudentUI/CharacterManagePanel.cs
+++ Assets/Scripts/UI/StudentUI/CharacterManagePanel.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterManagePanel : MonoBehaviour
 {
     [SerializeField] GameObject _characterBoxPrefab;
     [SerializeField] Transform _characterBoxParent;
+    [SerializeField] GameObject _backButtonObj;
 
     List<GameObject> boxList = new List<GameObject>();
 
@@ -13,7 +15,16 @@ public class CharacterManagePanel : MonoBehaviour
         for (int i = 0; i < StudentManager.Instance.MyStudents.Count; i++)
         {
             GameObject newBox = Instantiate(_characterBoxPrefab, _characterBoxParent);
-            newBox.GetComponent<CharacterBox>().Init(StudentManager.Instance.MyStudents[i]);
+            var cBox = newBox.GetComponent<CharacterBox>();
+
+            if (cBox != null)
+            {
+                cBox.Init(StudentManager.Instance.MyStudents[i]);
+                var btn = cBox.GetSelectButton();
+
+                btn.onClick.AddListener(delegate { _backButtonObj.SetActive(false); });
+            }
+        
             boxList.Add(newBox);
         }
     }
