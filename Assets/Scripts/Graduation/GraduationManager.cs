c34fb294438d851bdb7c29fe16d008c611c86707
gpwn145 <gpwn145@gmail.com>
2026-03-04 12:18:29 +0900

    feat: 졸업씬 졸업생 없을때 스킵 기능

--- Assets/Scripts/Graduation/GraduationManager.cs
+++ Assets/Scripts/Graduation/GraduationManager.cs
@@ -4,18 +4,18 @@ using UnityEngine.SceneManagement;
 
 public class GraduationManager : MonoBehaviour
 {
+    [SerializeField] private bool _isGraduationSkip;
+
     [SerializeField] private PromotionPanel _promotionPanel;
 
     [SerializeField] private List<Student> _myStudents;
 
     //졸업생 리스트
     [SerializeField] private List<Student> _graduationStudentList = new List<Student>();
-    //[SerializeField] private List<TestStudent> _testGraduationStudentList = new List<TestStudent>();
 
     //진급학생 리스트
     [SerializeField] private List<int> _promotionStudentList = new List<int>();
 
-    //[SerializeField] private List<TestStudent> _myTestStudents = new List<TestStudent>();
 
     private int _turn;
     private int _totalHonor;
@@ -27,6 +27,7 @@ public class GraduationManager : MonoBehaviour
     public List<Student> MyStudents => _myStudents;
     public int Turn { get { return _turn; } set { _turn = value; } }
     public int TotalHonor => _totalHonor;
+    public bool IsGraduationSkip => _isGraduationSkip;
 
     private void Start()
     {
@@ -37,7 +38,7 @@ public class GraduationManager : MonoBehaviour
             Debug.Log("학생 리스트없음");
         }
         ListCreat();
-
+        
         _turn = 0;
         //처음 학생 프로필 띄우기
         _promotionPanel.GetList();
@@ -61,6 +62,11 @@ public class GraduationManager : MonoBehaviour
                 _promotionStudentList.Add(_myStudents[i].StudentId);
                 Debug.Log($"{_myStudents[i].Name} : {_myStudents[i].Grade} 학년 진급생");
             }
+        }
+
+        if (_graduationStudentList.Count == 0)
+        {
+            _isGraduationSkip = true;
         }
     }
 
