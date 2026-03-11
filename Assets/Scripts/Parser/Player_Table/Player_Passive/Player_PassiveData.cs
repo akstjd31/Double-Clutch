74e6f466a721fe6788689f712add40732d5ebbb2
Songmin00 <dn7272@gmail.com>
2026-03-11 15:48:29 +0900

    fixed : PassiveExplainBox 설명 수정
    패시브 설명 부분의 {effectValue}를 실제 수치로 대체하여 표기.

--- Assets/Scripts/Parser/Player_Table/Player_Passive/Player_PassiveData.cs
+++ Assets/Scripts/Parser/Player_Table/Player_Passive/Player_PassiveData.cs
@@ -20,7 +20,7 @@ public enum effectType
     Rate2pt, Rate3pt, RateSteal, RateBlock, RatePass, RateRebound, //해당 스탯을 %로 상승
     Poten2pt, Poten3pt, PotenSteal, PotenBlock, PotenPass, PotenRebound, //해당 스탯 최대잠재력을 고정값 상승
     Growth2pt, Growth3pt, GrowthSteal, GrowthBlock, GrowthPass, GrowthRebound, //해당 스탯 증가 훈련 시 상승치 고정값 증가
-    GoldUp, ReputationUp, GraduationGold //골드 %상승, 졸업시 획득 명성 고정값, 졸업시 골드 획득 고정값    
+    MonthGoldUp, MatchGoldUp, ReputationUp, GraduationGold //골드 %상승, 졸업시 획득 명성 고정값, 졸업시 골드 획득 고정값    
 }
 
 [Serializable]
