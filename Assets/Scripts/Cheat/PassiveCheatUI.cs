7b96491c0ab4856e6af1c370047f73c997b541b6
Manseong Han <akstjd31@naver.com>
2026-03-19 17:28:15 +0900

    feat : 패시브 데이터 리스트 가져오기

--- Assets/Scripts/Cheat/PassiveCheatUI.cs
+++ Assets/Scripts/Cheat/PassiveCheatUI.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,32 +7,63 @@ public class PassiveCheatUI : MonoBehaviour
 {
     private Student _student;
     [SerializeField] private TMP_Dropdown _dropdown;
-    [SerializeField] private GameObject _passiveObj;
-    private TextMeshProUGUI[] _myPassives;
+    [SerializeField] private GameObject _passivesObj;
+    private TextMeshProUGUI[] _myPassiveTexts;
+    [SerializeField] List<string> skillNameData = new List<string>();
+    HashSet<string> nameData = new HashSet<string>();
 
-
-    private void OnEnable()
+    public void Init(Student std)
     {
+        _student = std;
+
+        if (_myPassiveTexts == null)
+        {
+            _myPassiveTexts = _passivesObj.GetComponentsInChildren<TextMeshProUGUI>();
+        }
+
+        foreach (var t in _myPassiveTexts)
+            t.text = "";
+        
+
+        for (int i = 0; i < _student.Passive.Count; i++)
+        {
+            _myPassiveTexts[i].text = StringManager.Instance.GetString(_student.Passive[i].skillName);
+        }
+
         if (_dropdown == null) return;
         if (_student == null) return;
-        if (_passiveObj == null) return;
+        if (_passivesObj == null) return;
 
-        if (_myPassives == null)
-        {
-            _myPassives = _passiveObj.GetComponentsInChildren<TextMeshProUGUI>();
-        }
-        
+        // 드롭다운 코드 작성 (모든 패시브 가져와서 선택하게끔)
         _dropdown.options.Clear();
 
-        // for (int i = 0; i < _student.Passive.Count; i++)
-        // {
-        //     var option = new TMP_Dropdown.OptionData();
-        //     option.text = StringManager.Instance.GetString(_student.Passive[i].skillName);
-        //     _dropdown.options.Add(option);
-        // }
+        if (StudentManager.Instance == null) return;
+        var passiveData = StudentManager.Instance.GetFactory().GetPassiveDataList();
+        
+        if (skillNameData == null)
+        {
+            for (int i = 0; i < passiveData.Count; i++)
+            {
+                skillNameData.Add(passiveData[i].skillName);
+            }
+        }
+
+        if (nameData == null)
+        {
+            for (int i = 0; i < skillNameData.Count; i++)
+            {
+                var passiveName = StringManager.Instance.GetString(skillNameData[i]);
+                nameData.Add(passiveName);
+            }
+        }
+
+        foreach (var n in nameData)
+        {
+            var option = new TMP_Dropdown.OptionData();
+            option.text = n;
+            _dropdown.options.Add(option);
+        }
 
         _dropdown.RefreshShownValue();
     }
-
-    public void SetStudent(Student std) => _student = std;
 }
