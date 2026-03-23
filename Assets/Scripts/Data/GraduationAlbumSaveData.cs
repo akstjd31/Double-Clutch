022ada0abc661390656d637059a8a22f974a1723
Manseong Han <akstjd31@naver.com>
2026-03-23 11:23:22 +0900

    feat : 졸업 데이터 검증 및 갱신 후 저장

--- Assets/Scripts/Data/GraduationAlbumSaveData.cs
+++ Assets/Scripts/Data/GraduationAlbumSaveData.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 // 졸업 앨범에 포함될 데이터
 public class GraduationAlbumSaveData : SaveBase
 {
-    List<GraduationStudent> graduationStudentList;
+    public List<GraduationStudent> graduationStudentList = new List<GraduationStudent>();
+
+    public GraduationAlbumSaveData() {}
+
+    public GraduationAlbumSaveData(GraduationStudent gStd)
+    {
+        graduationStudentList.Add(gStd);
+    }
 }
 
 // 기수별 학생 목록 데이터
@@ -12,4 +19,10 @@ public class GraduationStudent
 {
     public int graduatingClass;         // 기수
     public List<Student> studentList;
+
+    public GraduationStudent(int gClass, List<Student> stuList)
+    {
+        graduatingClass = gClass;
+        studentList = stuList;
+    }
 }
