e65ba5a52f32e1426121c9b4576d6a34825e250a
Manseong Han <akstjd31@naver.com>
2026-03-24 11:55:49 +0900

    feat : AddressableManager.cs
    - 어드레서블 관리 클래스 설계

--- Assets/Scripts/Manager/AddressableManager.cs
+++ Assets/Scripts/Manager/AddressableManager.cs
@@ -1,21 +1,62 @@
+using System;
 using UnityEngine;
+using System.Collections.Generic;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
-public class AddressableManager : MonoBehaviour
-{
-    AsyncOperationHandle<GameObject> instanceHandle;
 
-    private void Start()
+public class AddressableManager : Singleton<AddressableManager>
+{
+    private Dictionary<string, AsyncOperationHandle> _assetHandles = new Dictionary<string, AsyncOperationHandle>();
+    private Dictionary<GameObject, string> _instanceToAddress = new Dictionary<GameObject, string>();
+
+    public void LoadAsset<T>(string address, Action<T> onComplete) where T : UnityEngine.Object
     {
-        instanceHandle = Addressables.InstantiateAsync("이름");
+        if (_assetHandles.ContainsKey(address))
+        {
+            onComplete?.Invoke(_assetHandles[address].Convert<T>().Result);
+            return;
+        }
+
+        var handle = Addressables.LoadAssetAsync<T>(address);
+        handle.Completed += (op) =>
+        {
+            if (op.Status == AsyncOperationStatus.Succeeded)
+            {
+                _assetHandles[address] = op;
+                onComplete?.Invoke(op.Result);
+            }
+        };
     }
 
-    private void OnDestroy()
+    // UI 프리팹 생성
+    public void InstantiateUI(string address, Transform parent, Action<GameObject> onComplete)
     {
-        if (instanceHandle.IsValid())
+        var handle = Addressables.InstantiateAsync(address, parent);
+        handle.Completed += (op) =>
         {
-            Addressables.ReleaseInstance(instanceHandle);
+            if (op.Status == AsyncOperationStatus.Succeeded)
+            {
+                _instanceToAddress[op.Result] = address;
+                onComplete?.Invoke(op.Result);
+            }
+        };
+    }
+
+    // 메모리 해제
+    public void UnloadAsset(string address)
+    {
+        if (_assetHandles.TryGetValue(address, out var handle))
+        {
+            Addressables.Release(handle);
+            _assetHandles.Remove(address);
         }
+    }
+
+    public void ReleaseInstance(GameObject obj)
+    {
+        if (obj == null) return;
+        Addressables.ReleaseInstance(obj);
+        _instanceToAddress.Remove(obj);
     }
 }
