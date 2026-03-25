557981670cba6f5a5c78f3d775530dcb43cfae4f
thaud0000 <thaud3967@gmail.com>
2026-03-25 16:51:18 +0900

    fixed : 토너먼트 탈락시의 버그 대응

--- Assets/Scripts/UI/SimulatorUI/SwissBoardPanel.cs
+++ Assets/Scripts/UI/SimulatorUI/SwissBoardPanel.cs
@@ -296,11 +296,12 @@ public class SwissBoardPanel : MonoBehaviour
         var currentLeague = LeagueManager.Instance.CurrentLeague;
         _btnAction.onClick.RemoveAllListeners();
 
+        bool cannotPlay = currentLeague.isFinished || currentLeague.isPlayerEliminated;
         // 버튼 텍스트 세팅 수정
-        _txtBtnAction.text = string.IsNullOrEmpty(_customActionText) ? (currentLeague.isFinished ? StringManager.Instance.GetString("UI_Popup_닫기") : StringManager.Instance.GetString("UI_Matchlog_경기준비")) : _customActionText;
+        _txtBtnAction.text = string.IsNullOrEmpty(_customActionText) ? (cannotPlay ? StringManager.Instance.GetString("UI_Popup_닫기") : StringManager.Instance.GetString("UI_Matchlog_경기준비")) : _customActionText;
 
         // 리그가 완전히 종료된 상태 (수정: _customAction.Invoke() 추가)
-        if (currentLeague.isFinished)
+        if (cannotPlay)
         {
             _btnAction.interactable = true;
             _btnAction.onClick.AddListener(() =>
