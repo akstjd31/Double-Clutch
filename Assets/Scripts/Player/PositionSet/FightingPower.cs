d62872bcf81203904607d57924bc17e1c7541723
Manseong Han <akstjd31@naver.com>
2026-03-26 11:25:54 +0900

    chore : FightingPower.cs 주석문 제거

--- Assets/Scripts/Player/PositionSet/FightingPower.cs
+++ Assets/Scripts/Player/PositionSet/FightingPower.cs
@@ -139,52 +139,6 @@ public class FightingPower : MonoBehaviour
                 }
             }
         }
-        //if (generatedAwayTeam == null)
-        //{
-        //    Debug.LogError("적 팀 생성 실패");
-        //    return;
-        //}
-        //Debug.Log($"[생성 확인] 상대 1번 선수 2점슛 스탯: {generatedAwayTeam.Roster[0].GetStat(MatchStatType.TwoPoint)}");
-
-        //if (_rivalMatchingStudentList != null && _rivalMatchingStudentList.Count > 0)
-        //{
-        //    for (int i = 0; i < _rivalMatchingStudentList.Count; i++)
-        //    {
-        //        var r = _rivalMatchingStudentList[i];
-        //        r.RebuildStatDict();
-        //        _rivalList[i].Init(r);
-        //        _rivalTotalFightingPower += (r.Attack + r.Defense);
-        //    }
-        //}
-        //else
-        //{
-        //    for (int i = 0; i < _rivalList.Length; i++)
-        //    {
-        //        MatchPlayer mp = generatedAwayTeam.Roster[i];
-        //        Student rivalStudent = new Student();
-
-        //        rivalStudent.SetName(mp.PlayerName[0], mp.PlayerName[1], mp.PlayerName[2]);
-        //        rivalStudent.SetPosition(mp.MainPosition);
-
-        //        var mappedStats = new List<Stat>
-        //{
-        //    new Stat(potential.Stat2pt, mp.GetStat(MatchStatType.TwoPoint), 99, 1),
-        //    new Stat(potential.Stat3pt, mp.GetStat(MatchStatType.ThreePoint), 99, 1),
-        //    new Stat(potential.StatPass, mp.GetStat(MatchStatType.Pass), 99, 1),
-        //    new Stat(potential.StatBlock, mp.GetStat(MatchStatType.Block), 99, 1),
-        //    new Stat(potential.StatSteal, mp.GetStat(MatchStatType.Steal), 99, 1),
-        //    new Stat(potential.StatRebound, mp.GetStat(MatchStatType.Rebound), 99, 1)
-        //};
-
-        //        rivalStudent.SetStat(mappedStats);
-        //        rivalStudent.OnStatChanged();
-
-        //        _rivalList[i].Init(rivalStudent);
-        //        _rivalMatchingStudentList.Add(rivalStudent);
-
-        //        _rivalTotalFightingPower += (rivalStudent.Attack + rivalStudent.Defense);
-        //    }
-        //}
 
         _rivalSchoolName.text = StringManager.Instance.GetString(_generatedAwayTeam.TeamName);
         StringManager.Instance.ApplyFont(_rivalSchoolName);
@@ -197,18 +151,6 @@ public class FightingPower : MonoBehaviour
         _mySchoolName.text = GameManager.Instance.SaveData.schoolName;
         _myFightingPowerText.text = _myTotalFightingPower.ToString();
     }
-
-    //public void SaveRivalMachingStudentData()
-    //{
-    //    if (_rivalMatchingStudentList.Count < 1 || _rivalMatchingStudentList == null) return;
-
-    //    int rivalCnt = _rivalMatchingStudentList.Count;
-
-    //    var rivalData = new StudentSaveData(rivalCnt, _rivalMatchingStudentList);
-
-    //    if (SaveLoadManager.Instance == null) return;
-    //    SaveLoadManager.Instance.Save(FilePath.RIVAL_STUDENT_MATCHING_PATH, rivalData);
-    //}
 
     public void OnClickStartMatch()
     {
