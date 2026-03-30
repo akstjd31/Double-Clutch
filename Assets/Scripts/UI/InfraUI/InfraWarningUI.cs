ece565336961fa7c0aa4c85e5852c8218c381872
Songmin00 <dn7272@gmail.com>
2026-03-30 16:51:34 +0900

    fixed : 인프라 지원금 부족 경고문구 다국어 대응
    인프라 지원금 팝업 경고문구 다국어 대응 변환.

--- Assets/Scripts/UI/InfraUI/InfraWarningUI.cs
+++ Assets/Scripts/UI/InfraUI/InfraWarningUI.cs
@@ -8,14 +8,25 @@ public class InfraWarningUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _needCostText;
     [SerializeField] private Button _confirmButton;
 
+    int _needCost;    
+
     public void Init(InfraController iController)
     {
         int needCost = iController.GetCostByNextLevel() - GameManager.Instance.SaveData.money;
-        _needCostText.text = $"필요한 지원금: {needCost}";
+        _needCost = needCost;
+        _needCostText.text = StringManager.Instance.GetString("UI_Infra_지원금부족2").Replace("{N}", needCost.ToString());
+        StringManager.Instance.ApplyFont(_needCostText);
+    }
+
+    private void Refresh()
+    {
+        _needCostText.text = StringManager.Instance.GetString("UI_Infra_지원금부족2").Replace("{N}", _needCost.ToString());
+        StringManager.Instance.ApplyFont(_needCostText);
     }
 
     private void OnEnable()
     {
+        StringManager.OnLanguageChanged += Refresh;
         if (_confirmButton == null) return;
         _confirmButton.onClick.RemoveAllListeners();
         _confirmButton.onClick.AddListener(delegate
@@ -24,5 +35,10 @@ public class InfraWarningUI : MonoBehaviour
             AudioManager.Instance.PlaySoundOneShot(SoundName.SE_BUTTON_SELECT);
             this.gameObject.SetActive(false);
         });
+    }
+
+    private void OnDisable()
+    {
+        StringManager.OnLanguageChanged -= Refresh;
     }
 }
