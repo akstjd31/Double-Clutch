67b641af03d9adf42a843d90413e9092fa4c8c7f
thaud0000 <thaud3967@gmail.com>
2026-04-03 13:11:50 +0900

    fixed : 영입 리세마라 방지

--- Assets/Scripts/Data/StudentSaveData.cs
+++ Assets/Scripts/Data/StudentSaveData.cs
@@ -7,16 +7,18 @@ public class StudentSaveData : SaveBase
     public List<Student> studentList; // �� �л� ��� ��ü ����
     public Team currentTeam;
 
-    public StudentSaveData(int idCount, List<Student> students, Team currentTeam)
+    public List<Student> recruitCandidates = new List<Student>();
+    public StudentSaveData(int idCount, List<Student> students, Team currentTeam, List<Student> candidates = null)
     {
         this.lastIdCount = idCount;
         this.studentList = students;
         this.currentTeam = currentTeam;
+        this.recruitCandidates = candidates ?? new List<Student>();
     }
 
     // �⺻ ������ (JsonUtility �ε��)
     public StudentSaveData()
     {
-
+        recruitCandidates = new List<Student>();
     }
 }
