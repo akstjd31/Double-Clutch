db8df6f2b4a80e093fde825d14706c147df31520
Manseong Han <akstjd31@naver.com>
2026-04-03 10:18:55 +0900

    feat : 시즌아웃 팝업 추가
    - 팝업 확인 클릭 시 다음 주차 진행

---
+++ Assets/Scripts/SeasonOutChecker.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class SeasonOutChecker : MonoBehaviour
+{
+    [SerializeField] private GameObject _popupObj;
+
+    public void SetPopupActivate(bool active)
+    {
+        if (_popupObj == null) return;
+        _popupObj.SetActive(active);
+    }
+}
